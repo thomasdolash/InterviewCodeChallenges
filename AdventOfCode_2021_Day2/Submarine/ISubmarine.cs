13bcb22aba2479e4db093c4b6e84c9bc9b063ddf
Cory Brostowicz <61064638+cbrostowicz-prime@users.noreply.github.com>
2022-07-29 09:16:41 -0500

    - Updated Naming - Updated consturctor for full file test

--- AdventOfCode_2021_Day2/Submarine/ISubmarine.cs
+++ AdventOfCode_2021_Day2/Submarine/ISubmarine.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace Submarine {
     public interface ISubmarine {
-        public decimal CurrentTrajectoryV1 { get; }
+        public decimal Answer_Part1 { get; }
 
-        public decimal CurrentTrajectoryV2 { get; }
+        public decimal Answer_Part2 { get; }
     }
 }
