a6df040119a29bc25925e00b3de3bc9111226829
thomasdolash <thomasdolash@gmail.com>
2024-03-12 16:03:41 -0500

    Submit proposed solution to part 1 and part 2.

--- AdventOfCode_2021_Day2/Submarine/Submarine.cs
+++ AdventOfCode_2021_Day2/Submarine/Submarine.cs
@@ -13,6 +13,10 @@
 
         public Submarine(string InstructionFile = "input.txt") : this(File.ReadAllLines(InstructionFile)) { }
 
+        
+        // Direction 
+        private int aim = 0;
+
         /// <summary>
         /// Main Application Entry Point 
         /// 
@@ -21,7 +25,95 @@
         /// </summary>
         /// <param name="Instructions"></param>
         public Submarine(string[] Instructions) {
+            // Iterate upon the instruction set
+
+            ProcessInstructions_Simple(Instructions);
+            ProcessInstructions_Complex(Instructions);
+
+        }
+
+        private void ProcessInstructions_Simple(string[] Instructions)
+        {
+            // Submarine depth
+            int depth = 0;
+            // Distance traveled in the horizontal plane
+            int horizontalDistance = 0;
+
+            foreach (string Instruction in Instructions)
+            {
+                // Initialize our components with the input
+                string[] components = Instruction.Split(" ");
+
+                // get our command from our components
+                string command = components[0];
+                // get our magnitude from the components
+                int magnitude = int.Parse(components[1]);
+
+                switch (command)
+                {
+                    case "up":
+                        depth -= magnitude; // Go deeper
+                        aim -= magnitude;
+                        break;
+                    case "down":
+                        depth += magnitude; // Go more shallow
+                        aim += magnitude;
+                        break;
+                    case "forward":
+                        horizontalDistance += magnitude; // Go further forward/backwards
+                        break;
+                }
+            }
+
+            this.Answer_Part1 += depth * horizontalDistance;
+        }
+
+        private void ProcessInstructions_Complex(string[] Instructions)
+        {
+            int depth = 0;
+            int aim = 0;
+            int horizontalDistance = 0;
+
+            foreach (string Instruction in Instructions)
+            {
+                // Initialize our components with the input
+                string[] components = Instruction.Split(" ");
+
+                // get our command from our components
+                string command = components[0];
+                // get our magnitude from the components
+                int magnitude = int.Parse(components[1]);
+
+                switch (command)
+                {
+                    case "up":
+                        //depth -= magnitude; // Go deeper
+                        aim -= magnitude;
+                        break;
+                    case "down":
+                        //depth += magnitude; // Go more shallow
+                        aim += magnitude;
+                        break;
+                    case "forward":
+                        horizontalDistance += magnitude; // Go further forward/backwards
+                        // Increase depth by aim * instruction magnitude
+                        depth += aim * magnitude;
+                        break;
+                }
+            }
+
+            this.Answer_Part2 = depth * horizontalDistance;
+        }
+
+
+        /// <summary>
+        /// Process our inbound instruction in order to calculate our new depth and distance
+        /// </summary>
+        /// <param name="Instruction"></param>
+        private void ProcessInstruction(string Instruction)
+        {
             
+
         }
 
 
